6c3056401ce3b473e02c8cea1280f0e20dd5a05e
zoe-gonzales <zoegonzales03@gmail.com>
2019-05-20 16:35:01 -0600

    adding state into Main so that data from db can be displayed

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,23 +1,34 @@
-import React from "react";
-
+import React, { Component } from "react";
 import Find from "../Main/Search";
 import ImageHome from "../Main/ImageCard";
+import API from '../../utils/API';
 
+class Main extends Component {
+  state = {
+    animalsList: []
+  }
 
+  componentDidMount() {
+    API.getAllAnimals()
+       .then(res => {
+         console.log(res.data);
+         this.setState({ animalsList: res.data });
+       })
+       .catch(error => console.log(error));
+  }
 
-
-
-const Main = () => {
-
-  return (
-    <div id="ll"className="container">
-      <Find />
-      <br />
-      <ImageHome />
-
-      {/* <h1>This is the landing page after sign-in or sign-up(Animal Page)</h1> */}
-    </div>
-  )
+  render() {
+    return (
+      <div id="ll" className="container">
+        <Find />
+        <br />
+        {this.state.animalsList.map(animal => {
+          return <ImageHome animal={animal} />;
+        })}
+        {/* <h1>This is the main page after sign-in or sign-up(Animal Page)</h1> */}
+      </div>
+    )
+  }
 }
 
 export default Main;
