518bd3cf6fe711ad94bd13ba688256d6d976c2e1
Sarah-Git <45321654+Sarah-Git@users.noreply.github.com>
2019-05-21 19:10:01 -0600

    fixing links on the navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -11,18 +11,18 @@ function Navigator() {
   return (
 
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                 <Navbar.Brand href="#home">Journey's Shelter Management Application</Navbar.Brand>
+                 <Navbar.Brand href="/">Journey's Shelter Management Application</Navbar.Brand>
                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
                  <Navbar.Collapse id="basic-navbar-nav">
                  <Nav className="mr-auto">
-                 <Nav.Link href="#home">Home</Nav.Link>
-                 <Nav.Link href="#link">Link</Nav.Link>
+                 <Nav.Link href="/main">Home</Nav.Link>
+                 <Nav.Link href="/link">Link</Nav.Link>
                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                 <NavDropdown.Item href="/action/3.1">Action</NavDropdown.Item>
+                 <NavDropdown.Item href="/action/3.2">Another action</NavDropdown.Item>
+                 <NavDropdown.Item href="/action/3.3">Something</NavDropdown.Item>
                  <NavDropdown.Divider />
-                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                 <NavDropdown.Item href="/action/3.4">Separated link</NavDropdown.Item>
                  </NavDropdown>
                  </Nav>
                  <Form inline>
